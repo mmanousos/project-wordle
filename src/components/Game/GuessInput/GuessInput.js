d3b6d4a4eb3a4f6994c8e6e7865b332dfabdb27c
Melissa Manousos <mmanousos@gmail.com>
2024-06-07 14:59:21 -0700

    guess input

---
+++ src/components/Game/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const GuessInput = () => {
+  const [ guess, setGuess ] = React.useState('');
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(guess.toUpperCase());
+        setGuess('');
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        required={true}
+        pattern={'[a-zA-Z]{5}'}
+        value={guess}
+        onChange={(event) => setGuess(event.target.value)}
+        placeholder="five letters here..."
+      />
+    </form>
+  );
+};
+
+export default GuessInput;
